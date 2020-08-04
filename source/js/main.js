21ebbf92c5141347fefe6ec50824ec308189cd08
Suslyak <sergey.suslyak@mail.ru>
2020-08-04 03:43:49 +0300

    добавлен блок абонементов; добавлены некоторые цвета

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 (function () {
+  var AbonementTapsElement = document.querySelector(".abonements__taps");
+  var AbonementCardsListElement = document.querySelector(".abonements__cards-list");
+
   if (typeof window !== 'undefined' && window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = function (callback, thisArg) {
       thisArg = thisArg || window;
@@ -18,4 +21,7 @@
       }
     };
   }
+
+  AbonementTapsElement.classList.remove('abonements__taps--no-js');
+  AbonementCardsListElement.classList.remove('abonements__cards-list--no-js');
 })();
