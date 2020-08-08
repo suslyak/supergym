445ad93e2ec853d993ebfb7c49c2209b7da10253
Suslyak <sergey.suslyak@mail.ru>
2020-08-08 03:33:18 +0300

    js для абонементов

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 (function () {
-  var AbonementTapsElement = document.querySelector(".abonements__taps");
-  var AbonementCardsListElement = document.querySelector(".abonements__cards-list");
+  var AbonementTapsFormElement = document.querySelector('.abonements__taps');
+  var AbonementTapsRadioElements = AbonementTapsFormElement.querySelectorAll('input[name="mounth"]');
+  var AbonementCardsListElement = document.querySelector('.abonements__cards-list');
 
   if (typeof window !== 'undefined' && window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = function (callback, thisArg) {
@@ -22,6 +23,31 @@
     };
   }
 
-  AbonementTapsElement.classList.remove('abonements__taps--no-js');
+  AbonementTapsFormElement.classList.remove('abonements__taps--no-js');
   AbonementCardsListElement.classList.remove('abonements__cards-list--no-js');
+
+  var switchAbonements = function (data) {
+    var abonementCards = AbonementCardsListElement.querySelectorAll('.abonements__card');
+
+    abonementCards.forEach(function (card) {
+      if (parseInt(card.getAttribute('slider-data-mounth'), 10) !== parseInt(data, 10)) {
+        card.classList.add('abonements__card--hide');
+      } else {
+        card.classList.remove('abonements__card--hide');
+      }
+    });
+  };
+
+  var initAbonements = function () {
+    switchAbonements(1);
+  };
+
+  initAbonements(1);
+
+  AbonementTapsRadioElements.forEach(function (element) {
+    element.addEventListener('change', function () {
+      switchAbonements(element.value);
+    });
+  });
+
 })();
